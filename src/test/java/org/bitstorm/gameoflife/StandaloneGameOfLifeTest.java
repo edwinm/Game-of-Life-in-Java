b4093f5a951a545b50a8d0550173f06dec79db21
HanJongKo <rino0601.dev@gmail.com>
2017-11-12 18:28:28 +0900

    write build.gradle and  jUnit4 Test case example, modify ReadMe.md

---
+++ src/test/java/org/bitstorm/gameoflife/StandaloneGameOfLifeTest.java
@@ -0,0 +1,23 @@
+package org.bitstorm.gameoflife;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StandaloneGameOfLifeTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void exampleCase() throws Exception {
+        assertTrue(false);
+    }
+
+}
