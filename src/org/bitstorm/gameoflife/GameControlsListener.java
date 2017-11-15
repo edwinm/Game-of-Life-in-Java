dfe5162f69bee96f325ce0cea44ba202137c5c80
RedChiken <gombabo1217@gmail.com>
2017-11-15 16:41:34 +0900

    Change concrete object(GameOfLifeControlEvent) to abstract object(GameControlEvent)

--- src/org/bitstorm/gameoflife/GameControlsListener.java
+++ src/org/bitstorm/gameoflife/GameControlsListener.java
@@ -20,29 +20,29 @@ public interface GameControlsListener extends EventListener {
 	 * The Start/Stop button is clicked.
 	 * @param e event object
 	 */
-	public void startStopButtonClicked( GameOfLifeControlsEvent e );
+	public void startStopButtonClicked( GameControlsEvent e );
 
 	/**
 	 * The Next button is clicked.
 	 * @param e event object
 	 */
-	public void nextButtonClicked( GameOfLifeControlsEvent e );
+	public void nextButtonClicked( GameControlsEvent e );
 
 	/**
 	 * A new speed is selected.
 	 * @param e event object
 	 */
-	public void speedChanged( GameOfLifeControlsEvent e );
+	public void speedChanged( GameControlsEvent e );
 
 	/**
 	 * A new cell size is selected.
 	 * @param e event object
 	 */
-	public void zoomChanged( GameOfLifeControlsEvent e );
+	public void zoomChanged( GameControlsEvent e );
 
 	/**
 	 * A new shape is selected.
 	 * @param e event object
 	 */
-	public void shapeSelected( GameOfLifeControlsEvent e );
+	public void shapeSelected( GameControlsEvent e );
 }
